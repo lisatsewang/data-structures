01eb103c50f1251670373cce560de6a24196ffc4
Hack Reactor Students <accounts@hackreactor.com>
2015-07-01 16:36:08 -0700

    Complete up to functional shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,43 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.storage = {};
+  newQueue.length = 0;
+  newQueue.head = 1;
+  newQueue.tail = 0;
+
+  extend(newQueue, queueMethods);
+  return newQueue;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.length++;
+  this.tail++;
+  this.storage[this.tail] = value;
+};
 
+queueMethods.dequeue = function() {
+  var temp;
 
+  if (this.length > 0) {
+    this.length--;
+    temp = this.storage[this.head];
+    delete this.storage[this.head];
+    this.head++;
+  }
+
+  return temp;
+};
+
+queueMethods.size = function() {
+  return this.length;	
+};
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,31 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
+  var head = 1;
+  var tail = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    length++;
+    tail++;
+    storage[tail] = value;
   };
 
   someInstance.dequeue = function(){
+    var value;
+    if (length > 0) {
+      value = storage[head];
+      delete storage[head];
+      head++;
+      length--;
+    }
+    return value;
   };
 
   someInstance.size = function(){
+    return length;
   };
 
   return someInstance;
